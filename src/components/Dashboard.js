c83d4681ed1c896b49360fa4bcd233fe70ee6590
Cassie/Ren <cassandra.chen@uoit.net>
2022-01-27 17:49:57 -0500

    first commit, write mapping/filter values

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,78 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 import classnames from "classnames";
+import Loading from "./loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6,
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm",
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday",
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3",
+  },
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  state = {
+    loading: false,
+    focused: null,
+  };
 
-    return <main className={dashboardClasses} />;
+  Dashboard(props) {
+    const [state, setState] = React.useState({ focused: null });
+
+    function selectPanel(id) {
+      setState({
+        focused: id,
+      });
+    }
+  }
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+    const { id, label, value, onSelect } = this.props;
+    const panel = data
+      .filter(
+        (props) =>
+          this.state.focused === null || this.state.focused === props.id
+      )
+      .map((props) => (
+        <Panel
+          key={props.id}
+          id={props.id}
+          label={props.label}
+          value={props.value}
+          onSelect={this.selectPanel}
+        />
+      ));
+    <section
+      className="dashboard__props"
+      onClick={(event) => this.onSelect(id)}
+    >
+      {panel}
+    </section>;
+    return <main className={dashboardClasses}></main>;
   }
 }
-
 export default Dashboard;
